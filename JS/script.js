fd421cc4aea59326843fef0710ddd4e9ae6355a3
Mike Sanders <msanders331@gmail.com>
2022-01-23 07:52:45 -0800

    Changed the html structure of the timeblocks to a form and added bootstrap classes/styling. Added ToDOs to the JS file.

--- JS/script.js
+++ JS/script.js
@@ -1,6 +1,13 @@
 var $currentDayEl = $('#currentDay');
-var today = moment();
-console.log(moment());
+var today = moment().local();
 
+//displays current day/time at the top of the page
 $currentDayEl.text(today.format("dddd, MMMM Do YYYY"));
 
+//TODO Determine standard business hours
+
+//TODO Time block color code - past, present, or future
+
+//TODO Events are clickable and typeable text
+
+//TODO Events are stored in Local Storage

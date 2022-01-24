e3adb990e6a1487d840483f30dee66b19e217e30
Mike Sanders <msanders331@gmail.com>
2022-01-24 07:11:30 -0800

    Added classes for past present and future for changing the text background based on what time of day it is right now. Need to scale the solution all pieces are ready for scaling.

--- JS/script.js
+++ JS/script.js
@@ -1,7 +1,12 @@
-var $currentDayEl = $('#currentDay');
+//Moment.js Variables
 var today = moment().local();
+var currentHour = moment().hour();
+
+//jQuery variables
+var $currentDayEl = $('#currentDay');
 var $button9AM= $('#button-9AM');
 var $text9AM = $('#9AM');
+
 
 //displays current day/time at the top of the page
 $currentDayEl.text(today.format("dddd, MMMM Do YYYY"));
@@ -10,13 +15,17 @@ $currentDayEl.text(today.format("dddd, MMMM Do YYYY"));
 
 //TODO Time block color code - past, present, or future
 
+var timeBlockedColor = function(){
+    $text9AM.addClass('background-past');
+}
+
 //TODO Events are stored in Local Storage
+//Loop through the event creation and local storage using an array?
 
 $button9AM.on('click', function(event){
     event.preventDefault();
     var text = $text9AM.val();
-    localStorage.setItem('9AM', text)
-
+    localStorage.setItem('9AM', text);
 });
 
 var newText = localStorage.getItem('9AM');

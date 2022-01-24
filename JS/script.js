bc17fab6d94602d7d1e1bb97587e39a591a93ca0
Mike Sanders <msanders331@gmail.com>
2022-01-24 06:39:08 -0800

    Solved the storing text into local storage. Needs to be scaled across all available time blocks. Moving on to color coding then scaling the solution to the other time blocks.

--- JS/script.js
+++ JS/script.js
@@ -1,5 +1,7 @@
 var $currentDayEl = $('#currentDay');
 var today = moment().local();
+var $button9AM= $('#button-9AM');
+var $text9AM = $('#9AM');
 
 //displays current day/time at the top of the page
 $currentDayEl.text(today.format("dddd, MMMM Do YYYY"));
@@ -8,6 +10,15 @@ $currentDayEl.text(today.format("dddd, MMMM Do YYYY"));
 
 //TODO Time block color code - past, present, or future
 
-//TODO Events are clickable and typeable text
-
 //TODO Events are stored in Local Storage
+
+$button9AM.on('click', function(event){
+    event.preventDefault();
+    var text = $text9AM.val();
+    localStorage.setItem('9AM', text)
+
+});
+
+var newText = localStorage.getItem('9AM');
+
+$text9AM.val(newText);

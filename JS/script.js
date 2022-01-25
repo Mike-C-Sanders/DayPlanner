07c02bef9e0c4fcd7f601e6e91e5dccd169871ee
Mike Sanders <msanders331@gmail.com>
2022-01-24 21:37:36 -0800

    added createEventListener function. Purpose is to build a click event listener for each time entry slot. Once the save button is clicked then the local storage will save the item to it's time value key.

--- JS/script.js
+++ JS/script.js
@@ -4,12 +4,14 @@ var currentHour = moment().hour();
 
 //jQuery variables
 var $currentDayEl = $('#currentDay');
-var $button9AM= $('#button-9AM');
+// var $button9AM= $('#button-9AM');
 var $text9AM = $('#9AM');
 
 //Other Global Variables
-var textIDArr = ['9AM', '10AM', '11AM', '12PM', '1PM', '2PM', '3PM', '4PM', '5PM'];
+var buttonID = '#button-';
+var timeArray = ['9AM', '10AM', '11AM', '12PM', '1PM', '2PM', '3PM', '4PM', '5PM'];
 
+var $button9AM = $(buttonID+timeArray[0]);
 //displays current day/time at the top of the page
 $currentDayEl.text(today.format("dddd, MMMM Do YYYY"));
 
@@ -19,15 +21,31 @@ var timeBlockedColor = function(){
     $text9AM.addClass('background-past');
 }
 
-//TODO Events are stored in Local Storage
-//Loop through the event creation and local storage using an array?
+//function create event listener for each textarea. 
+var createEventListeners = function(){
+    //Loop through each time array element and apply event listeners
+    timeArray.forEach(function(i){
+        var $listener = $(buttonID+i);
+        var textID = $('#'+i);
 
-$button9AM.on('click', function(event){
-    event.preventDefault();
-    var text = $text9AM.val();
-    localStorage.setItem('9AM', text);
-});
+        //Event Listener on Save button
+        $listener.on('click', function(event){
+            event.preventDefault();
+            var text = textID.val();
+            console.log(text);
+            //store items locally
+            localStorage.setItem(i, text);
+        });
+    })
+}
+
 
 var newText = localStorage.getItem('9AM');
 
 $text9AM.val(newText);
+
+createEventListeners();
+
+
+
+
